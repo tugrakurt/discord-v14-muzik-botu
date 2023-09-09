f249bdeecd9323f6f10965bb2e2b402a2d44caac
tugrakurt <144524758+tugrakurt@users.noreply.github.com>
2023-09-09 11:59:55 +0300

    ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "MTE0OTYzMTUyOTI3OTA0MTU4Nw.GRLmNh.r4dFbXXD-DP7NywyxFX6g6mbqR1SE66fQOgL6w",  // bot token gir :)
     PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    SAHİP: process.env.SAHİP || "702770588284092566", //sahip id :()
 }
